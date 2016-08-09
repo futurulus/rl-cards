066d19765972c14e2042394f881a10eb93cd564e
Will Monroe <wmonroe4@stanford.edu>
2016-08-08 21:14:30 -0700

    Update env_display to use CardsWorld

--- env_display.py
+++ env_display.py
@@ -1,6 +1,7 @@
 import gym
 import cards_env
 import cards_cache
+import world
 
 from stanza.research.rng import get_rng
 
@@ -12,8 +13,7 @@ if __name__ == '__main__':
 
     for transcript in cards_cache.all_transcripts():
         env.reset()
-        new_trans = transcript
-        env.configure(new_trans, verbosity=2)
+        env.configure(world.CardsWorld(transcript), verbosity=2)
         for _ in range(100):
             env.render()
             dirs = ['right', 'up', 'left', 'down']
